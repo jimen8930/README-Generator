64412cbb7eb959b6452b574e30b594757e4c5fee
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-06-09 14:44:26 -0400

    Added an array of  questions that will be prompted.

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,54 @@ const fs =require('fs');
 const inquirer =require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: "Title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: 'input',
+        name: "Description",
+        message: "Write a brief description of your project?",
+    },
+    {
+        type: 'input',
+        name: "Installation",
+        message: "Describe the process needed to install your project?",
+    },
+    {
+        type: 'input',
+        name: "Usage",
+        message: "What is the usage information needed for this project?",
+    },
+    {
+        type: 'list',
+        name: "License",
+        message: "What kind of license was used for this project?",
+        choices: ['GNU, MIT, APACHE 2.0, BSD 3, GPL 3.0, None'],
+    },
+    {
+        type: 'input',
+        name: "test",
+        message: "What command is needed to run tests?",
+    },
+    {
+        type: 'input',
+        name: "Contribute",
+        message: "Who were the contributors for this project?",
+    },
+    {
+        type: 'input',
+        name: "username",
+        message: "Please enter your username for Github?",
+    }, 
+    {
+        type: 'input',
+        name: "email",
+        message: "Please enter your email? ",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
