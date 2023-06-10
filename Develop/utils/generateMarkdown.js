58b6ff271d5869b58e5a6df4abd64a19613fefd6
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-06-10 16:48:13 -0400

    Added conditional statements for license links to return

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -20,7 +20,23 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return 'https://opensource.org/licenses/MIT';
+  }
+  else if (license === "GPL") {
+    return 'https://www.gnu.org/licenses/gpl-3.0'
+  }
+  else if (license === 'APACHE 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
+  }
+  else if (license === 'BSD 3') {
+    return 'https://opensource.org/licenses/BSD-3-Clause';
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
