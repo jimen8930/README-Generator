dfb2411a5d5fe39fdb3428982c9d31655b0ad852
Elizabeth Jimenez <2good2btru@Elizabeths-MacBook-Air-3.local>
2023-06-10 17:15:02 -0400

    Added content for markdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -61,6 +61,43 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseLink(data.license)}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+To run tests, use the following command:
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+If you have any questions, feel free to reach out to me:
+- GitHub: [@${data.username}](https://github.com/${data.username})
+- Email: ${data.email}
 
 `;
 }
